56fb362055cf26ae4ffcad6b65dcd58a0417fe78
Heiner <heiner.peuser@outlook.com>
2020-04-27 09:40:09 +0200

    Limited precision

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 while true
 do
         cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
-        cpu=$(echo "$cpu/1000" | bc -l)
+        cpu=$(echo "scale=2; $cpu/1000" | bc -l)
         echo "Current temperature: $cpu °C"
         mosquitto_pub -h $HOST -p $PORT -u $USER -P $PASS -t $TOPIC/cpu/temperature -r -m $cpu
         sleep $INTERVAL
